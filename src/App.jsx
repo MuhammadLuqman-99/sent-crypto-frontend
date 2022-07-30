ecc5e1fefa43cf2caf11d9c1c2a7bf87914c229a
Muhammad Luqman <92778267+MuhammadLuqman-99@users.noreply.github.com>
2022-07-31 00:03:03 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { Navbar, Welcome, Footer, Loader, Services, Transactions } from './components'
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-const App = () => {
-  return (
-    <div className="min-h-screen">
-            <div className="gradient-bg-welcome">
-                <Navbar/>
-                <Welcome/>
-            </div>
-            <Services/>
-            <Transactions/>
-            <Footer/>
-        </div>
-  )
-}
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-export default App
-
-
+export default App;
