43822007a1223d410511c987b4e80af3699ea433
Muhammad Luqman <92778267+MuhammadLuqman-99@users.noreply.github.com>
2022-07-31 15:13:46 +0800

    update @

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import { Navbar, Welcome, Footer, Services, Transactions } from "@/components"
-
-=======
-import { Navbar, Welcome, Footer, Services, Transactions } from "src/components"
->>>>>>> d9e26cc33e164a057c3ab48b4b39546e2af1ce2a
 
 const App = () => (
   <div className="min-h-screen">
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App.jsx";
-import { TransactionsProvider } from "./context/TransactionContext.jsx";
+import App from "@/App.jsx";
+import { TransactionsProvider } from "@/context/TransactionContext.jsx";
 import "./index.css";
 
 ReactDOM.render(
